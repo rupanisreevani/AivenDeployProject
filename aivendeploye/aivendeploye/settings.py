ce739f07c4c96924f497b73bb4cad26705411050
rupanisreevani <rupanisreevani@gmail.com>
2025-12-16 15:43:35 +0530

    allowedlist

--- aivendeploye/aivendeploye/settings.py
+++ aivendeploye/aivendeploye/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aivendeployproject-3-k084.onrender.com','127.0.0.1']
 
 
 # Application definition
